ba9017860abf4217ceaec9366c3ba13907d5f516
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-24 13:10:00 +0100

    reading analog signal and display to screen

---
+++ main/main.ino
@@ -0,0 +1,20 @@
+#define joyY A1
+#define joyX A0
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+int xValue, yValue;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+	xValue = analogRead(joyX);
+	yValue = analogRead(joyY);
+
+
+	Serial.print(xValue);
+	Serial.print("\t");
+	Serial.print(yValue);
+  Serial.print("\n");
+}
